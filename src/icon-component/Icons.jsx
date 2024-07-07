96f4351f4a163503383c5cefaefaae3dd1f2c32d
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-07-07 16:34:48 +0100

    icons

--- src/icon-component/Icons.jsx
+++ src/icon-component/Icons.jsx
@@ -1355,3 +1355,23 @@ export const RingSmall = () => {
     </svg>
   );
 };
+
+export const ModalCancel = () => {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 13L13 1M1 1L13 13"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
