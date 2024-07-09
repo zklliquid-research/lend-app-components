0eec64bad787fb109ec616d5d217dbdefe390fc9
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2024-07-09 17:46:05 +0100

    custom bgcolor

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "custom-gradient": "linear-gradient(to left, #4DFFDF 0%, #4DA1FF 100%)",
+      },
       fontFamily: {
         Poppins: ["Poppins", "sans-serif"],
         Raleway: ["Raleway", "sans-serif"],
